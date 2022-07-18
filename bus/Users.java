1081233e8fd5a0ad87ba274e2578a3aa0dcee0dd
vaibhav-js <vaibhavrajpianist@gmail.com>
2022-07-18 16:03:57 +0530

    added new functionalities

--- bus/Users.java
+++ bus/Users.java
@@ -1,14 +1,5 @@
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Users {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
     private Contact contact;
     private Address address;
     private String password;
